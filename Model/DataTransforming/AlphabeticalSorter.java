16c94a8f3feb6ed51d03eee0d7a849cf904ed638
Glenda <gsalas1@uco.edu>
2021-02-26 11:38:07 -0600

    updated rules in alphabet sorter

--- Model/DataTransforming/AlphabeticalSorter.java
+++ Model/DataTransforming/AlphabeticalSorter.java
@@ -9,8 +9,8 @@ public class AlphabeticalSorter implements Sorter {
     public ArrayList<String> start(ArrayList<String> stmt) {
 
         String rules = ("< a < A < b < B < c < C < d < D < e < E < f < F < g < G"
-                + "G < h < H <i < I < j < J < k < K < l < L < m < M < n < N < o < O < "
-                + "P < q < Q < r < R < s < S < t < T < u < U < v < V < w < W < x < X <y < Y < z < Z");
+                + "G < h < H <i < I < j < J < k < K < l < L < m < M < n < N < o < O < p"
+                + "p < P < q < Q < r < R < s < S < t < T < u < U < v < V < w < W < x < X <y < Y < z < Z");
 
         try {
             RuleBasedCollator collator = new RuleBasedCollator(rules);
