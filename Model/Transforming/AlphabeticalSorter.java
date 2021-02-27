be1c8165592580868a989e3ac687ba12a393855a
Glenda <gsalas1@uco.edu>
2021-02-26 22:01:35 -0600

    partially fixed sorting bug

--- Model/Transforming/AlphabeticalSorter.java
+++ Model/Transforming/AlphabeticalSorter.java
@@ -8,9 +8,7 @@ public class AlphabeticalSorter implements Transformer {
 
     public ArrayList<String> start(ArrayList<String> stmt) {
 
-        String rules = ("< a < A < b < B < c < C < d < D < e < E < f < F < g < G"
-                + "G < h < H <i < I < j < J < k < K < l < L < m < M < n < N < o < O < p"
-                + "p < P < q < Q < r < R < s < S < t < T < u < U < v < V < w < W < x < X <y < Y < z < Z");
+        String rules = (" < a < A < b < B < c < C < d < D < e < E < f < F < g < G < h < H <i < I < j < J < k < K < l < L < m < M < n < N < o < O < p < P < q < Q < r < R < s < S < t < T < u < U < v < V < w < W < x < X <y < Y < z < Z");
 
         try {
             RuleBasedCollator collator = new RuleBasedCollator(rules);
